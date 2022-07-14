6f4f31f33552a00b2d71f5a50538c67c047aaa04
osidekyle <hawkinskyle67@gmail.com>
2022-07-13 22:41:25 -0700

    basic naive bayes classifier, saved model in pickle'

--- text_classifier.py
+++ text_classifier.py
@@ -1,6 +1,7 @@
 import nltk
 import random
 from nltk.corpus import movie_reviews
+import pickle
 
 documents = [(list(movie_reviews.words(fileid)), category)
              for category in movie_reviews.categories()
@@ -17,7 +18,34 @@ for w in movie_reviews.words():
     all_words.append(w.lower())
 
 all_words = nltk.FreqDist(all_words)
-#print(all_words.most_common(15))
 
-print(all_words['stupid'])
+word_features = list(all_words.keys())[:3000]
 
+
+def find_features(document):
+    words = set(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
+
+    return features
+
+#print(find_features(movie_reviews.words("neg/cv000_29416.txt")))
+
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
+training_set = featuresets[:1900]
+testing_set = featuresets[1900:]
+
+# classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+classifier_f = open("naive_bayes.pickle", "rb")
+classifier = pickle.load(classifier_f)
+classifier_f.close()
+
+print("Naive Bayes Algo accuracy:", (nltk.classify.accuracy(classifier, testing_set))*100)
+classifier.show_most_informative_features(15)
+
+# save_classifier = open("naive_bayes.pickle", "wb")
+# pickle.dump(classifier, save_classifier)
+# save_classifier.close()
