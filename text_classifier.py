c7342e3b09f28ebf3364f9452bfe9ec7d8d34bf3
osidekyle <hawkinskyle67@gmail.com>
2022-07-14 17:53:48 -0700

    used different classifiers and voted best classifier

--- text_classifier.py
+++ text_classifier.py
@@ -2,6 +2,33 @@ import nltk
 import random
 from nltk.corpus import movie_reviews
 import pickle
+from nltk.classify.scikitlearn import SklearnClassifier
+from nltk.classify import ClassifierI
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+from statistics import mode
+
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        self.classifiers = classifiers
+
+    def classify(self, features):
+        votes = []
+        for c in self.classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        return mode(votes)
+
+    def confidence(self, features):
+        votes = []
+        for c in self.classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
+
 
 documents = [(list(movie_reviews.words(fileid)), category)
              for category in movie_reviews.categories()
@@ -43,9 +70,58 @@ classifier_f = open("naive_bayes.pickle", "rb")
 classifier = pickle.load(classifier_f)
 classifier_f.close()
 
-print("Naive Bayes Algo accuracy:", (nltk.classify.accuracy(classifier, testing_set))*100)
+print("Original Naive Bayes Algo accuracy:", (nltk.classify.accuracy(classifier, testing_set))*100)
 classifier.show_most_informative_features(15)
+
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+print("MNB Naive Bayes Algo accuracy:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+
+# GaussianNB_classifier = SklearnClassifier(GaussianNB())
+# GaussianNB_classifier.train(training_set)
+# print("Gaussian Naive Bayes Algo accuracy:", (nltk.classify.accuracy(GaussianNB_classifier, testing_set))*100)
+
+BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+BernoulliNB_classifier.train(training_set)
+print("Bernoulli Naive Bayes Algo accuracy:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+
 
 # save_classifier = open("naive_bayes.pickle", "wb")
 # pickle.dump(classifier, save_classifier)
 # save_classifier.close()
+
+# LogisticRegression,SGDClassifier
+# SVC, LinearSVC, NuSVC
+
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_classifier.train(training_set)
+print("Logistic Regression Algo accuracy:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+
+SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+SGDClassifier_classifier.train(training_set)
+print("SGDC Algo accuracy:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
+
+SVC_classifier = SklearnClassifier(SVC())
+SVC_classifier.train(training_set)
+print("SVC Algo accuracy:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+print("Linear SVC Algo accuracy:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+
+NuSVC_classifier = SklearnClassifier(NuSVC())
+NuSVC_classifier.train(training_set)
+print("NuSVC Algo accuracy:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
+
+voted_classifier = VoteClassifier(classifier,
+                                  MNB_classifier,
+                                  BernoulliNB_classifier,
+                                  LogisticRegression_classifier,
+                                  SGDClassifier_classifier,
+                                  SVC_classifier,
+                                  LinearSVC_classifier,
+                                  NuSVC_classifier)
+
+print("Voted Algo accuracy:", (nltk.classify.accuracy(voted_classifier, testing_set))*100)
+
+print("Classification: ", voted_classifier.classify(testing_set[0][0]), "Confidence:", voted_classifier.confidence(testing_set[0][0]))
